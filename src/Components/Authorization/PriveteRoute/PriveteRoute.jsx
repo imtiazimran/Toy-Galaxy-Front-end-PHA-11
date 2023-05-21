d736eb45cb24c700912c8eea3a8975d4c4972a49
Imran <imtiazimran@outlook.com>
2023-05-21 19:40:06 +0600

    aos installed

--- src/Components/Authorization/PriveteRoute/PriveteRoute.jsx
+++ src/Components/Authorization/PriveteRoute/PriveteRoute.jsx
@@ -2,6 +2,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import { Spinner } from "flowbite-react";
 
 
 const PriveteRoute = ({children}) => {
@@ -9,10 +10,14 @@ const PriveteRoute = ({children}) => {
     const from = useLocation()
     console.log(from)
 
-    const {user} = useContext(AuthContext)
+    const {user, isLoading} = useContext(AuthContext)
 
     if(user){
         return children
+    }
+
+    if(isLoading){
+        return <div className="bg-white text-center"><Spinner aria-label="Center-aligned spinner example" /></div>
     }
 
     return <Navigate to="/login" state={from}></Navigate>;
--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,19 +1,32 @@
+import { useEffect } from "react";
+import 'aos/dist/aos.css';
 import Slider from "./Carousel";
 import ImageGallary from "./ImageGallary";
 import NewArrivel from "./NewArrivel";
 import ToyBrandLogos from "./OurPartnars";
 import Tab from "./Tab";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 
 
 const HomePage = () => {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 6000,
+            offset: 100,
+        });
+    }, []);
+
     return (
         <div className="w-10/12 mx-auto">
-            <Slider></Slider>
-            <ImageGallary></ImageGallary>
-            <Tab></Tab>
-            <NewArrivel></NewArrivel>
-            <ToyBrandLogos></ToyBrandLogos>
+            <Slider data-aos="fade-up"></Slider>
+            <ImageGallary data-aos="fade-up" data-aos-delay="200"></ImageGallary>
+            <Tab data-aos="fade-up"></Tab>
+            <NewArrivel data-aos="fade-up"></NewArrivel>
+            <ToyBrandLogos data-aos="fade-up"></ToyBrandLogos>
         </div>
     );
 };
